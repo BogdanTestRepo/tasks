9a0e418a0c620d522629d8176c33bf206965d67b
BogdanTestRepo <dar-bodya@ya.ru>
2017-03-01 15:18:13 +0200

    Update start_tasks3-4.sh

--- start_tasks3-4.sh
+++ start_tasks3-4.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v /home/bogdan/Ardas/jenkins_data:/var/jenkins -v /var/run/docker.sock:/var/run/docker.sock dockerdar/jenkins
+sudo docker run -d -p 8080:8080 -p 50000:50000 --name jenkins -v $(pwd)/jenkins_data:/var/jenkins -v /var/run/docker.sock:/var/run/docker.sock dockerdar/jenkins
